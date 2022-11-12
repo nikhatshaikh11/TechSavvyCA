23997fedb9daada81f799d99ccf4add7810af86d
Nikhat <nikhat.shaikhh11@gmail.com>
2022-11-12 23:37:39 +0530

    logoChanged

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,16 +4,15 @@ import './footer.css'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer__logo'>TechSavvyCA.</a>
-
+      {/* <a href="#" className='footer__logo'>TechSavvyCA.</a> */}
+      <h1>TechSavvyCA.</h1>
+{/* 
       <ul className='permalinks'>
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
-        {/* <li><a href="#experience">Experience</a></li> */}
         <li><a href="#dashboard">Dashoard</a></li>
-        {/* <li><a href="#testimonials">Testimonials</a></li> */}
         <li><a href="#contact">Contact</a></li>
-      </ul>
+      </ul> */}
 
       {/*
       <div className="footer__socials">
